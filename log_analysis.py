0b2f4b67960195331b95aa10a691de0ba5d58eb1
Edgar Jr. San Martin <esanmartinjr@fordham.edu>
2018-07-13 03:01:17 -0400

    Update log_analysis.py

--- log_analysis.py
+++ log_analysis.py
@@ -7,37 +7,43 @@ db_name = "news"
 
 
 query_one = "1.What are the most popular three articles of all time?\n"
-query_one_ans = ("SELECT articles.title, COUNT(*) AS num "
-                "FROM articles JOIN log ON log.path "
-                "LIKE concat('/article/%', articles.slug) "
-                "GROUP BY articles.title "
-                "ORDER BY num DESC "
-                "LIMIT 3;")
+query_one_ans = """
+                SELECT articles.title, COUNT(*) AS num
+                FROM articles JOIN log ON log.path
+                LIKE concat('/article/%', articles.slug)
+                GROUP BY articles.title
+                ORDER BY num DESC
+                LIMIT 3;
+                """
 
 query_two = "2.Who are the most popular article authors of all time?\n"
-query_two_ans = ("SELECT authors.name, COUNT(*) AS num "
-                 "FROM authors "
-                 "JOIN articles ON authors.id = articles.author "
-                 "JOIN log ON log.path like concat('/article/%', articles.slug) "
-                 "GROUP BY authors.name "
-                 "ORDER BY num DESC "
-                 "LIMIT 4;")
+query_two_ans = """
+                SELECT authors.name, COUNT(*) AS num
+                FROM authors
+                JOIN articles ON authors.id = articles.author
+                JOIN log ON log.path like concat('/article/%', articles.slug) 
+                GROUP BY authors.name 
+                ORDER BY num DESC 
+                LIMIT 4;
+                """
 
 query_three = "3.On which days did more than 1% of requests lead to errors?\n"
-query_three_ans = ("SELECT total.day, "
-                   "ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent "
-                   "FROM ("
-                   "SELECT date_trunc('day', time) 'day', count(*) AS error_requests "
-                   "FROM log "
-                   "WHERE status LIKE '404%' "
-                   "GROUP BY day) AS errors "
-                   "JOIN ("
-                   "SELECT date_trunc('day', time) 'day', count(*) AS requests "
-                   "FROM log "
-                   "GROUP BY day) AS total "
-                   "ON total.day = errors.day "
-                   "WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01) "
-                   "ORDER BY percent DESC;")
+query_three_ans = """
+                  SELECT total.day,
+                  ROUND(((errors.error_requests*1.0) / total.requests), 3) AS percent
+                  FROM (
+                  SELECT date_trunc('day', time) 'day', count(*) AS error_requests
+                  FROM log
+                  WHERE status LIKE '404%'
+                  GROUP BY day) AS errors
+                  JOIN (
+                  SELECT date_trunc('day', time) 'day', count(*) AS requests
+                  FROM log
+                  GROUP BY day) AS total
+                  ON total.day = errors.day
+                  WHERE (ROUND(((errors.error_requests*1.0) / total.requests), 3) > 0.01)
+                  ORDER BY percent DESC;
+                  """
 
 
 # Boilerplate code for query requests with psycopg2
